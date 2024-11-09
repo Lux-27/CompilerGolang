5064694cf70453b091d13ff3aa3be42a8d54ecf4
Lux-27 <raj170303@gmail.com>
2024-11-09 14:13:38 +0530

    backtrack from InterpreterGolang

---
+++ repl/repl.go
@@ -0,0 +1,57 @@
+package repl
+
+import (
+	"CompilerGolang/evaluator"
+	"CompilerGolang/lexer"
+	"CompilerGolang/object"
+	"CompilerGolang/parser"
+	"bufio"
+	"fmt"
+	"io"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
+	macroEnv := object.NewEnvironment()
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		evaluator.DefineMacros(program, macroEnv)
+		expanded := evaluator.ExpandMacros(program, macroEnv)
+
+		evaluated := evaluator.Eval(expanded, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
+
+const ErrorMsg = ` Arcadium is ....dyinnngg---dead`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, ErrorMsg)
+	io.WriteString(out, "Woops! Something went poof!!\n")
+	io.WriteString(out, "parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
